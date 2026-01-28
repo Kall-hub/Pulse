b84a6cf4062b862191528f51175b821edb8469b9
Kall-hub <kallymashigo3@gmail.com>
2026-01-28 05:53:03 +0200

    Fix: Move 'use client' directive to top of all loading.js files

--- src/app/helpdesk/loading.js
+++ src/app/helpdesk/loading.js
@@ -1,5 +1,6 @@
 "use client";
 import PulseLoader from "../components/PulseLoader";
+
 export default function Loading() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-[#F1F5F9]">
